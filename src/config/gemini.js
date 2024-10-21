f9bef5895a8cb46e2b48ab5394c811f218a94496
Roopesh N <roopeshnagineni09apr@gmail.com>
2024-10-21 01:41:41 -0400

    geminiApiInitialization

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -7,7 +7,6 @@ const {
   
 const apiKey = process.env.GEMINI_API_KEY;
   const genAI = new GoogleGenerativeAI(apiKey);
-  
   const model = genAI.getGenerativeModel({
     model: "gemini-1.5-flash",
   });
